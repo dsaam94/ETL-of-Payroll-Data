c9f09d1451fe78ab19623f12783961c47ada46c7
dsaam94 <35396741+dsaam94@users.noreply.github.com>
2018-01-16 14:03:36 +0500

    Add files via upload

---
+++ ODS Transformations and Jobs/payroll_ods_employees.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: payroll_ods
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES ('E01','Kashif Bashir','D1','G2'),('E02','Feroz Ali Meghani','D2','G1'),('E03','Syed Faraz Haider Naqvi','D3','G3'),('E04','Ghulam Muhammad','D4','G4'),('E05','Syed Nasir Danial','D2','G1'),('E06','Usman Waheed','D1','G1'),('E07','Rizwan ur Rashid','D2','G2'),('E08','Farooq Ali Khan','D3','G1'),('E09','Humera Ghani','D4','G3'),('E10','Khalid Mehmood','D2','G4'),('E11','M. Arshad Baig Mughal','D1','G1'),('E12','M. Shahbaz Khan','D2','G1'),('E13','M. Umair Naqvi','D3','G2'),('E14','Rauf Ahmed Shams Malick','D4','G1'),('E15','S. Khawaja Amanullah','D2','G3'),('E16','Twaha Ahmed Minai','D1','G4'),('E17','Muhammad Imran ','D2','G1'),('E18','Emad Abdullah Aziz','D3','G1'),('E19','Ikramullah Khan','D4','G2'),('E20','Sohail Imran','D2','G1'),('E23','Raza Kamal','D3','G1'),('E24','Muntazir H. Rizvi','D4','G1'),('E25','Hasan Perrveiz','D2','G2'),('E26','Farooq Rasheed','D1','G1'),('E27','Sabrina Saeed','D2','G3'),('E28','Kanwar Akhtar Ali','D3','G4'),('E29','Amir Feroz Shamsi','D4','G1'),('E30','Khalil Ahmed','D2','G1'),('E31','Muhammad Sarfraz','D1','G2'),('E32','Zafar Mehdi ','D2','G1'),('E33','M. Ramzan Ali','D3','G3'),('E34','Babar Saad Khairi','D4','G4'),('E35','Rakhshinda Ali','D2','G1'),('E36','Zia Dost Muhammad','D1','G1'),('E37','Mahrukh Shuja','D2','G2'),('E38','M. A. Khan','D3','G1'),('E39','Shams Naveed','D4','G3'),('E42','Aliya Nazir','D2','G1'),('E43','Atika Kidwai','D3','G2'),('E44','Farhan Farhat Siddiqui','D4','G1'),('E45','Illahi Bux Brohi','D2','G3'),('E46','Imran Hayder','D1','G4'),('E47','M. Ayaz Ahmed','D2','G1'),('E48','M. Sajid Ali','D3','G1'),('E49','M. Shahbaz Hussain','D4','G2'),('E50','Muhammad Afzal','D2','G1'),('E51','Mumtaz Ali Siddiqui ','D1','G3'),('E52','Najam Munir','D2','G4'),('E53','Shahzad Halani','D3','G1'),('E54','Rashid Hussain','D4','G1'),('E55','S. Samar Abbas Naqvi ','D2','G2'),('E56','Saghir Hussain','D1','G1'),('E57','Saqib Khan','D2','G3'),('E58','Sheeraz Khursheed','D3','G4'),('E59','Tariq Mahmood Khan','D4','G1'),('E60','Tariq Saeed Tanoli','D2','G1'),('E61','Tasnim Tariq','D1','G2');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-16 13:57:42
